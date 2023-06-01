25693e840b5661de6db6275b019f5b6c253bdfa1
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-06-01 20:54:33 -0300

    update-3

--- src/views/PagesProduc/PagesProduc.jsx
+++ src/views/PagesProduc/PagesProduc.jsx
@@ -4,6 +4,7 @@ import { CardProduc } from "../../components/CardProduc/CardProduc.jsx";
 import { useParams } from "react-router-dom";
 import ImgRemera1 from "../../assets/remera.png"
 import ImgPantalon1 from "../../assets/pantalon.png"
+import ImgPantalonSkiny from "../../assets/pantalonSkinny.png";
 
 const produc = [
     {
@@ -16,10 +17,10 @@ const produc = [
     },
     {
         id: "2",
-        title: "pantalon SlimFit",
+        title: "pantalon skinny",
         descrip: "lorem lore jasd ifns jdsojdfsdf hjd d djsdofh",
         catego: "pantalones",
-        img: ImgPantalon1,
+        img: ImgPantalonSkiny,
         price: "320"
     },
     {
@@ -39,14 +40,23 @@ export function PagesProduc(){
 
     useEffect(() => {
         
-            /* console.log("estas en " + idcate); */
-        if(!idcate){
-            setValue(value);
+        if(idcate == "todos"){
+            /* setValue(value); */
+            console.log("estas en " + idcate);
         
-        }else{
+        }
+        else if(idcate == 'pantalones'){
             /* console.log("no estas en categoria"); */
             setValue(value.filter(prod => prod.catego == idcate))
-            console.log(value)
+            /* console.log(value) */
+        }
+        else if(idcate == 'remeras'){
+            setValue(value.filter(prod => prod.catego == idcate))
+            /* console.log(value) */
+        }
+        else if(idcate == 'buzos'){
+            setValue(value.filter(prod => prod.catego == idcate))
+            /* console.log(value) */
         }
     },[idcate])
 
